9b703488d6840147a65319031012b7f09eab3381
dalilagiu98 <148708941+dalilagiu98@users.noreply.github.com>
2024-03-20 22:28:18 +0100

    created class event

--- U4-W3-D2/src/main/java/org/example/entities/Event.java
+++ U4-W3-D2/src/main/java/org/example/entities/Event.java
@@ -1,5 +1,31 @@
 package org.example.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+@Entity
 public class Event {
-    
+
+    //ATTRIBUTES LIST:
+    @Id
+    @GeneratedValue
+    private long id;
+    private String title;
+    private LocalDate eventDate;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private EventType eventType;
+    private int maxNumberParticipants;
+
+
+    //CONSTRUCTORS:
+    public Event() {}
+    public Event(String title, LocalDate eventDate, String description, EventType eventType, int maxNumberParticipants) {
+       this.title = title;
+       this.eventDate = eventDate;
+       this.description = description;
+       this.eventType = eventType;
+       this.maxNumberParticipants = maxNumberParticipants;
+    }
+
 }
